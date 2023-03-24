a31de1e12338bed9c6b07de25b437532bcf1ea18
Alikhan-Batyr <115405716+Alikhan9292@users.noreply.github.com>
2023-03-24 19:30:19 +0600

    8?
    AGAINx8?

--- quiz/quizizz/questions/models.py
+++ quiz/quizizz/questions/models.py
@@ -1,26 +1,49 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 class Quiz(models.Model):
-    name = models.CharField(max_length=255)
-    description = models.TextField()
-    content = models.TextField(blank=True)
-    photo = models.ImageField(upload_to="photos/%Y/%m/%d/")
-    time_create = models.DateTimeField(auto_now_add=True)
-    time_update = models.DateTimeField(auto_now=True)
-    is_published = models.BooleanField(default=True)
+    name = models.CharField(max_length=255, verbose_name="Заголовок")
+    description = models.TextField(verbose_name="Описание")
+    content = models.TextField(blank=True,  verbose_name="Текст статьи")
+    photo = models.ImageField(upload_to="photos/%Y/%m/%d/", verbose_name="Фото")
+    time_create = models.DateTimeField(auto_now_add=True,  verbose_name="Время создания")
+    time_update = models.DateTimeField(auto_now=True, verbose_name="Время изменения")
+    is_published = models.BooleanField(default=True, verbose_name="Публикация")
+
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('quiz-detail', kwargs={'pk': self.pk})
+
+    class Meta:
+        verbose_name = 'Квиз-задачи'
 
 class Question(models.Model):
     quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
     text = models.TextField()
+
+    class Meta:
+        verbose_name = 'Вопросы'
 
 class Answer(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     text = models.CharField(max_length=255)
     is_correct = models.BooleanField()
 
+
+    class Meta:
+        verbose_name = 'Ответ'
+
+
 class QuizTaker(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
     score = models.IntegerField()
     date_taken = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Составитель викторин'
+
